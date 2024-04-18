2e64182671b93be22a371acfa1b27fffae303cbd
gsilber <silber@udel.edu>
2024-04-18 14:31:02 -0400

    cone

---
+++ week10/race/src/app/obstacle/obstacle.component.ts
@@ -0,0 +1,9 @@
+import { EzComponent } from "@gsilber/webez";
+import html from "./obstacle.component.html";
+import css from "./obstacle.component.css";
+
+export class ObstacleComponent extends EzComponent {
+    constructor() {
+        super(html, css);
+    }
+}
